4598242fb3f5598df744d075d174c0de9f86cfd6
briancol <briancol07@gmail.com>
2020-04-11 17:08:25 -0300

    00-cHelloWorld

---
+++ 00-cHelloworld/hello.c
@@ -0,0 +1,18 @@
+/*
+	Name: Brian Colman
+	Date: 11/04/20 16:54
+	Description: primer programa Hello world 
+	
+	
+*/
+
+
+#include<stdio.h>
+
+int main(void){
+	
+printf("Hello World \n");
+
+return 0 ;
+
+}
